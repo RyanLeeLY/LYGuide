8fa6916fd4d2415459d923b144e9efb1a01f588b
liyao <liyao1021@163.com>
2016-10-30 13:58:34 +0800

    todo: cornerradius and coordinate

--- LYIntroductionDemoUITests/LYIntroductionDemoUITests.m
+++ LYIntroductionDemoUITests/LYIntroductionDemoUITests.m
@@ -16,7 +16,6 @@
 
 - (void)setUp {
     [super setUp];
-    
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
     // In UI tests it is usually best to stop immediately when a failure occurs.
